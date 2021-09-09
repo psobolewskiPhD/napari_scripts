85d3b1241f448c195f08d1c421370cac89b114e6
Peter Sobolewski <pete.sd@gmail.com>
2021-09-09 13:40:00 +0200

    Initial LIF Browser Widget

---
+++ Browse_LIF_widget.py
@@ -0,0 +1,67 @@
+#%%
+from aicsimageio import AICSImage
+from vispy.color import Colormap
+from qtpy.QtWidgets import QListWidget, QAbstractItemView, QListWidgetItem
+from qtpy.QtCore import Qt, QUrl, QFileInfo
+import napari
+
+
+#%%
+class SceneList(QListWidget):
+    def __init__(self, viewer, parent=None):
+        super().__init__(parent)
+        self.viewer = viewer
+        self.setAcceptDrops(True)
+        self.setDragEnabled(True)
+        self.currentItemChanged.connect(self.open_scene)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+
+            for url in event.mimeData().urls():
+                img_file = str(url.toLocalFile())
+                info = QFileInfo(img_file)
+                if info.completeSuffix() == "lif":
+                    # Load data from LIF into a dask stack
+                    img = AICSImage(img_file, reconstruct_mosaic=False)
+                    for scene in img.scenes:
+                        self.addItem(scene)
+                    self.img = img
+                else:
+                    event.ignore()
+        else:
+            event.ignore()
+
+    def open_scene(self):
+        item = self.currentItem()
+        scene = item.text()
+        self.img.set_scene(scene)
+        scale = [self.img.physical_pixel_sizes.Y, self.img.physical_pixel_sizes.X]
+        self.viewer.add_image(
+            self.img.get_image_dask_data("CZYX"), name=scene, scale=scale
+        )
+
+
+#%%
+def lif_widget():
+    viewer = napari.Viewer()
+    list_widget = SceneList(viewer)
+    viewer.window.add_dock_widget([list_widget], area="right")
+    return viewer
+
+
+#%%
