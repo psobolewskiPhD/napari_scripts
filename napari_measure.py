870825cf71923891281d297bc78102f528d32e2c
Peter Sobolewski <pete.sd@gmail.com>
2022-05-18 17:15:31 +0200

    Add support for measuring a selected shape

--- napari_measure.py
+++ napari_measure.py
@@ -7,11 +7,10 @@ import napari
 import numpy as np
 
 if TYPE_CHECKING:
-    import napari.layers
-    import napari.viewer
+    import napari.layers, napari.viewer
 
 
-def measure_shape(keybind="m"):
+def measure_shape(keybind:str="m")->None:
     viewer = napari.current_viewer()
     # get top-most, visible image layer
     img_layer = None
@@ -19,7 +18,7 @@ def measure_shape(keybind="m"):
         if isinstance(layer, napari.layers.Image) and layer.visible != 0:
             img_layer = layer
     if img_layer is None:
-        raise Exception("No image layer found; is needed for scale informtion")
+        raise Exception("No image layer found; is needed for scale information")
 
     # get img dimensions
     px_size = img_layer.scale[-2:]
@@ -44,38 +43,46 @@ def measure_shape(keybind="m"):
     def measure_prop(viewer: napari.Viewer) -> None:
         layer = viewer.layers["Measure"]
         if layer.data:
-            if layer.shape_type[-1] == "line":
-                length = math.dist(*layer.data[-1]) * px_size[-1]
+            # Check if a shape is selected, if so use that shape
+            if layer.selected_data:
+                m_shape_vertices = layer.data[[*layer.selected_data][0]]
+                m_shape_type = layer.shape_type[[*layer.selected_data][0]]
+            else: # otherwise use the last shape
+                m_shape_vertices = layer.data[-1]
+                m_shape_type = layer.shape_type[-1]
+                
+            if m_shape_type == "line":
+                length = math.dist(*m_shape_vertices) * px_size[-1]
                 print(f"Length: {round(length, 1)} {units}")
-            elif layer.shape_type[-1] == "rectangle":
+            elif m_shape_type == "rectangle":
                 area = (
-                    math.dist(layer.data[-1][0], layer.data[-1][1])
-                    * math.dist(layer.data[-1][0], layer.data[-1][3])
+                    math.dist(m_shape_vertices[0], m_shape_vertices[1])
+                    * math.dist(m_shape_vertices[0], m_shape_vertices[3])
                     * px_size[-1] ** 2
                 )
                 print(f"Area: {round(area)} {units}\u00b2")
-            elif layer.shape_type[-1] == "ellipse":
+            elif m_shape_type == "ellipse":
                 area = (
                     math.pi
                     * 0.5
-                    * math.dist(layer.data[-1][0], layer.data[-1][1])
+                    * math.dist(m_shape_vertices[0], m_shape_vertices[1])
                     * 0.5
-                    * math.dist(layer.data[-1][0], layer.data[-1][3])
+                    * math.dist(m_shape_vertices[0], m_shape_vertices[3])
                 ) * px_size[-1] ** 2
                 print(f"Area: {round(area)} {units}\u00b2")
-            elif layer.shape_type[-1] == "polygon":
-                i = np.arange(len(layer.data[-1]))
+            elif m_shape_type == "polygon":
+                i = np.arange(len(m_shape_vertices))
                 area = (
                     0.5
-                    * np.abs(np.sum(np.cross(layer.data[-1][i - 1], layer.data[-1][i])))
+                    * np.abs(np.sum(np.cross(m_shape_vertices[i - 1], m_shape_vertices[i])))
                     * px_size[-1] ** 2
                 )
                 print(f"Area: {round(area)} {units}\u00b2")
-            elif layer.shape_type[-1] == "path":
+            elif m_shape_type == "path":
                 length = 0
-                for i in range(len(layer.data[-1]) - 1):
+                for i in range(len(m_shape_vertices) - 1):
                     length += (
-                        math.dist(layer.data[-1][i], layer.data[-1][i + 1])
+                        math.dist(m_shape_vertices[i], m_shape_vertices[i + 1])
                         * px_size[-1]
                     )
                 print(f"Length: {round(length, 1)} {units}")
