647a8a2167596d7d5049fa1ec37df82cd10fbbd9
Peter Sobolewski <pete.sd@gmail.com>
2022-05-18 13:51:17 +0200

    Add polygons and paths

--- napari_measure.py
+++ napari_measure.py
@@ -1,18 +1,25 @@
-import napari
+from __future__ import annotations
+
 import math
+from typing import TYPE_CHECKING
+
+import napari
+import numpy as np
+
+if TYPE_CHECKING:
+    import napari.layers
+    import napari.viewer
 
 
 def measure_shape(keybind="m"):
     viewer = napari.current_viewer()
     # get top-most, visible image layer
-
-    def get_image_layer():
-        for layer in viewer.layers:
-            if isinstance(layer, napari.layers.Image) and layer.visible != 0:
-                img_layer = layer
-        return img_layer
-
-    img_layer = get_image_layer()
+    img_layer = None
+    for layer in viewer.layers:
+        if isinstance(layer, napari.layers.Image) and layer.visible != 0:
+            img_layer = layer
+    if img_layer is None:
+        raise Exception("No image layer found; is needed for scale informtion")
 
     # get img dimensions
     px_size = img_layer.scale[-2:]
@@ -34,7 +41,7 @@ def measure_shape(keybind="m"):
     measure_layer.mode = "add_line"
 
     @viewer.bind_key(keybind)
-    def measure_length(viewer):
+    def measure_prop(viewer: napari.Viewer) -> None:
         layer = viewer.layers["Measure"]
         if layer.data:
             if layer.shape_type[-1] == "line":
@@ -56,7 +63,23 @@ def measure_shape(keybind="m"):
                     * math.dist(layer.data[-1][0], layer.data[-1][3])
                 ) * px_size[-1] ** 2
                 print(f"Area: {round(area)} {units}\u00b2")
+            elif layer.shape_type[-1] == "polygon":
+                i = np.arange(len(layer.data[-1]))
+                area = (
+                    0.5
+                    * np.abs(np.sum(np.cross(layer.data[-1][i - 1], layer.data[-1][i])))
+                    * px_size[-1] ** 2
+                )
+                print(f"Area: {round(area)} {units}\u00b2")
+            elif layer.shape_type[-1] == "path":
+                length = 0
+                for i in range(len(layer.data[-1]) - 1):
+                    length += (
+                        math.dist(layer.data[-1][i], layer.data[-1][i + 1])
+                        * px_size[-1]
+                    )
+                print(f"Length: {round(length, 1)} {units}")
             else:
-                print("not a line, rectangle, or ellipse")
+                print("Not a line, path, rectangle, polygon, or ellipse")
         else:
-            print("no shapes")
+            print("No shape drawn yet")
